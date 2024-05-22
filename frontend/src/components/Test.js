3d0a35a61bcf973669571e9cf13680a04425199a
codecorneres <codecorneres@gmail.com>
2024-05-22 12:47:02 +0530

    today commit

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -4,24 +4,30 @@ function Test(){
   const [text, setText] = useState('');
   const [input, setInput] = useState('');
   const [increament, setIncreament] = useState(0);
+  const [stop, setStop] = useState(false);
 
+  const [name, setName] = useState('');
+
+ const addName = (name) => {
+  setName(name);
+ }
+  const stoped = () => {
+    setStop(false);
+  }
   const clicked = () => {
-    if (increament < 20) {
-      setIncreament( 
-        increament + 1
-      );
-    } else {
-      setIncreament(0)
-    }
-   
+    setStop(true)
   }
 
     useEffect(() => {
-      
-      setTimeout(() => {
-        clicked()
-      }, 500);
-    }, [increament]);
+      let interval;
+      if (stop) {
+        interval = setInterval(() => {
+          setIncreament(increament + 1)
+        }, 500);
+        
+      }
+      return () => clearInterval(interval)
+    }, [increament, stop]);
  
   const handlChange = (e) => {
     // console.log(e.target.value, 'ttt')
@@ -37,14 +43,25 @@ function Test(){
   return (
     <>
 
-     <div>
-     <h1>{increament}</h1>
-     <button className="" onClick={clicked}>Increament</button>
-     </div>
+    <div className='counter-wrap'>
+      <h1>{increament}</h1>
+      <button className="" onClick={clicked}>Play</button>
+      <button className="" onClick={stoped}>Stop</button>
+    </div>
 
-    <input type='text' name='name' className='text' onChange={handlChange} value={input} />
-    <button className="" onClick={renderText}>button</button>
-    <p>{text}</p>
+    <div className='render-text-wrap'>
+      <input type='text' name='name' className='text' onChange={handlChange} value={input} />
+      <button className="" onClick={renderText}>button</button>
+      <p>{text}</p>
+    </div>
+
+    <div className='add-clicked-btn-name-wrap'>
+      <h1>My name is {name}</h1>
+      <div className='text-btns'>
+        <button className="" onClick={() => addName('Suraj')}>Suraj</button>
+        <button className="" onClick={() => addName('Rajat')}>Rajat</button>
+      </div>
+    </div>
     </>
   );
   
